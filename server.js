3f155083cd77b10a933be3d268076a676fe9406f
sviveros05@gmail.com <sviveros05@gmail.com>
2019-10-18 15:48:29 -0500

    Schema and Model developed

--- server.js
+++ server.js
@@ -40,7 +40,12 @@ mongoose.connection.on('error', (err) => {
 });
 
 
+const dogSchema = new mongoose.Schema({
+	dog_name: {type: String required: true},
+	dog_breed: {type: String required: true}
+});
 
+const Dog = mongoose.model('Dog', dogSchema);
 
 
 

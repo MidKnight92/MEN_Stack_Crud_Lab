340e0948ebfc685415f9f3758dc0dd0537de7deb
sviveros05@gmail.com <sviveros05@gmail.com>
2019-10-18 15:02:21 -0500

    Rendered appNew

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const methodOverride = require('method-override');
 const bodyParser = require('body-parser');
 const PORT = 3000;
 
-
+app.get('/dogs/new', (req, res) => {
+	res.render('new.ejs');
+})
 
 app.listen(PORT, () => {
 	console.log('Listening on '+ PORT);
